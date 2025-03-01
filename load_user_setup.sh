1d5fae7670d85559920deb9279def8d354acd6d4
unknown <calvyn.morin@gmail.com>
2025-03-01 15:49:22 +0100

    second

---
+++ load_user_setup.sh
@@ -0,0 +1,3 @@
+wget -qO- https://raw.githubusercontent.com/Frozenka/Exegol-Ressources/refs/heads/main/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/calvyngit/Exegol/refs/heads/main/install.sh | bash
+
